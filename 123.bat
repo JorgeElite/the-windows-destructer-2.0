350b7e5c5e058e8887320a0d67d8f9281b9008eb
Jorge <76011221+JorgeElite@users.noreply.github.com>
2021-03-17 09:52:50 -0600

    Add files via upload

---
+++ 123.bat
@@ -0,0 +1,10 @@
+@echo off
+
+color 07
+
+
+:start
+
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+
+goto start
